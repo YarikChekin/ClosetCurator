0aec052ca1f5207a74e479d1e4e677b689961eec
Yarik Chekin <ychekin@gmail.com>
2025-05-04 22:48:08 -0700

    Initial scaffold

---
+++ ClosetCurator/ClosetCurator/ClosetCuratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetCuratorApp.swift
+//  ClosetCurator
+//
+//  Created by Yarik Chekin on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ClosetCuratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
